5d91d1463051f87aabb8058532673ffd90ac10bc
brandonmonty07-cyber <brandonmonty07@gmail.com>
2026-04-23 14:35:17 -0700

    #

--- Project1/Tower.hpp
+++ Project1/Tower.hpp
@@ -2,10 +2,11 @@
 
 #include <SFML/Graphics.hpp>
 
-class Tower : public sf::CircleShape
+class Tower : public sf::RectangleShape
 {
 public:
-    Tower(const sf::Vector2f& pos, const float& Damage = 0.0, const float& AS = 0.0, const float& Range = 0.0) : sf::CircleShape(30)
+    Tower(const sf::Vector2f& pos, const float& Damage = 0.0, const float& AS = 0.0, const float& Range = 0.0) 
+        : sf::RectangleShape(sf::Vector2f(50, 80))
     {
         //not sure if this is how you want the constructor or not
 
@@ -17,6 +18,7 @@ public:
 
         this->mRange = Range;
     }
+
 
     //setters
     void setDamage(float newDamage);
@@ -32,11 +34,15 @@ public:
 
     float getRange();
 
-private:
+protected:
 
     float mDamage;
 
     float mAS; //AS stands for attack speed (easier to write using AS. Can change if you would like to)
 
     float mRange;
+    
+    //size of the tower, can be used to change the size of the tower in the future if needed
+
+
 };
