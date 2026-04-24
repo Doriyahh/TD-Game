377375ff59d924607bcf57953dd9736a32535e24
brandonmonty07-cyber <brandonmonty07@gmail.com>
2026-04-24 01:49:09 -0700

    #

--- Project1/Tower.cpp
+++ Project1/Tower.cpp
@@ -1,31 +1,64 @@
 #include "Tower.hpp"
 
+void Tower::update(float dt)
+{
+    // Base update just ticks the shoot timer down
+    // Subclasses will override this to actually shoot
+    if (mShootTimer > 0.0f)
+    {
+        mShootTimer -= dt;
+    }
+}
+
+void Tower::drawRange(sf::RenderWindow& window)
+{
+    window.draw(mRangeCircle);
+}
+
+bool Tower::isTooClose(const Tower& other) const
+{
+    // Get distance between this tower and another
+    sf::Vector2f diff = this->getPosition() - other.getPosition();
+    float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
+
+    return distance < PLACEMENT_RADIUS;
+}
+
 void Tower::setDamage(float newDamage)
 {
-	this->mDamage = newDamage;
+    this->mDamage = newDamage;
 }
 
 void Tower::setAS(float newAS)
 {
-	this->mAS = newAS;
+    this->mAS = newAS;
 }
 
 void Tower::setRange(float newRange)
 {
-	this->mRange = newRange;
+    this->mRange = newRange;
+
+    // Keep range circle in sync when range is updated
+    mRangeCircle.setRadius(newRange);
+    mRangeCircle.setOrigin(sf::Vector2f(newRange, newRange));
 }
 
-float Tower::getDamage()
+float Tower::getDamage() const
 {
-	return this->mDamage;
+    return this->mDamage;
 }
 
-float Tower::getAS()
+float Tower::getAS() const
 {
-	return this->mAS;
+    return this->mAS;
 }
 
-float Tower::getRange()
+float Tower::getRange() const
 {
-	return this->mRange;
+    return this->mRange;
+}
+
+sf::CircleShape Tower::getRangeCircle() const
+{
+    return this->mRangeCircle;
 }
--- Project1/Tower.hpp
+++ Project1/Tower.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 class Tower : public sf::RectangleShape
 {
 public:
-    Tower(const sf::Vector2f& pos, const float& Damage = 0.0, const float& AS = 0.0, const float& Range = 0.0) 
-        : sf::RectangleShape(sf::Vector2f(50, 80))
+    Tower(const sf::Vector2f& pos, const float& Damage = 0.0f, const float& AS = 0.0f, const float& Range = 0.0f, const float& ShootTimer = 0.0f) 
+		: sf::RectangleShape(sf::Vector2f(50, 80)) // can change the size of the tower here if you want 
         {
         //not sure if this is how you want the constructor or not
 
@@ -17,23 +18,47 @@ public:
         this->mAS = AS;
 
         this->mRange = Range;
+
+        this->mShootTimer = ShootTimer;
+
+        // Set up the range circle - centered on tower, radius = mRange
+        mRangeCircle.setRadius(Range);
+        // use the circle's own radius as a vector origin (SFML expects a Vector2f)
+        mRangeCircle.setOrigin(sf::Vector2f(Range, Range));
+        mRangeCircle.setPosition(pos);
+        mRangeCircle.setFillColor(sf::Color(100, 100, 255, 50));   // transparent blue fill
+        mRangeCircle.setOutlineColor(sf::Color(100, 100, 255, 150)); // slightly visible outline
+        mRangeCircle.setOutlineThickness(1.0f);
+
         }
+
+    // Called each frame to handle shooting logic - virtual so subclasses can override
+    virtual void update(float dt);
+
+    // Draws the range circle to the window (call when tower is selected or placing)
+    virtual void drawRange(sf::RenderWindow& window);
+
+    // Returns true if another tower is within the minimum placement distance
+    bool isTooClose(const Tower& other) const;
     
-
-
     //setters
     void setDamage(float newDamage);
 
     void setAS(float newAS);
 
-    void setRange(float newRange);
+    void setRange(float newRange); // Also updates the range circle to match the new range
 
     //getters
-    float getDamage();
+    float getDamage() const;
 
-    float getAS();
+    float getAS() const;
 
-    float getRange();
+    float getRange() const;
+
+    sf::CircleShape getRangeCircle() const;
+
+    // Minimum distance allowed between two towers on placement
+    static constexpr float PLACEMENT_RADIUS = 60.0f;
 
 protected:
 
@@ -41,9 +66,10 @@ protected:
 
     float mAS; //AS stands for attack speed (easier to write using AS. Can change if you would like to)
 
-    float mRange;
-    
-    //size of the tower, can be used to change the size of the tower in the future if needed
+    float mRange; // shooting radius in pixels
 
+	float mShootTimer; //timer for shooting, tracks time between shots
+
+	sf::CircleShape mRangeCircle; //circle that shows the range of the tower
 
 };
