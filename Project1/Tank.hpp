15e43e10c28df6be3be29a74e3cd83a551972e86
Torres, Joshua <wearingkoala64@gmail.com>
2026-04-27 15:31:06 -0700

    #Addded Fast Enemy and Tank Enemy

---
+++ Project1/Tank.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "Enemy.hpp"
+
+class TankEnemy : public Enemy
+{
+public: TankEnemy(Game*& mainGame, const std::vector<sf::Vector2f>& wp) :
+	Enemy(mainGame, 5.f, 5.f, 1.f, 1.f, 6.f)
+{
+
+
+	this->mwaypoints = wp;
+	this->setPosition(sf::Vector2f(0.f, 137.5f));
+	this->setRadius(35);
+	this->setOrigin(sf::Vector2f(35, 35));
+	this->setFillColor(sf::Color::Blue);
+	this->mArmor = 3.0f;
+
+}
+
+	  ~TankEnemy()
+	  {
+		  if (this->getHealth() <= 0) {
+			  this->mGame->removeEnemy(this);
+		  }
+	  }
+
+
+protected:
+	void move() override
+	{
+		Enemy::move();
+	}
+
+
+};
